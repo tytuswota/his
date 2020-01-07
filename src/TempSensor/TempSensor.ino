c51438bd0122c2d4faa420d1977b7d49210bed78
Don Luyendijk <donluyendijk@gmail.com>
2020-01-07 22:20:36 +0100

    added temp sensor code (not calibrated)

---
+++ src/TempSensor/TempSensor.ino
@@ -0,0 +1,18 @@
+void toDeg(float r) {
+  // read = 0 - 1023
+  // V = read / (1023 / 5)
+  // mV = V * 1000
+  // deg = mV / 10
+
+  // deg = read/2.046
+  return r/2.046;
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+}
+
+void loop() {
+  Serial.println(toDeg(analogRead(A0)));
+}
