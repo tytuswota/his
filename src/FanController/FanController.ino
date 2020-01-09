78e508047ea38ac8bbf245fb3e7a7e74bc1be947
Don Luyendijk <donluyendijk@gmail.com>
2020-01-09 13:35:31 +0100

    now uses interrupts

--- src/FanController/FanController.ino
+++ src/FanController/FanController.ino
@@ -1,12 +1,21 @@
-unsigned long toRpm(unsigned long msphr) {
-  return 60000/(msphr*0.002);
+unsigned long toRpm(unsigned long pulseTime) {
+  return 60/(pulseTime*0.002);
+}
+
+unsigned long pulseTime;
+
+void isr() {
+  static unsigned long start = 0;
+  if(!digitalRead(2)) start = millis();
+  if(digitalRead(2)) pulseTime = millis()-start;
 }
 
 void setup() {
+  attachInterrupt(digitalPinToInterrupt(2), isr, CHANGE);
   pinMode(2, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println(toRpm(pulseIn(2, LOW)));
+  Serial.println(toRpm(pulseTime));
 }
