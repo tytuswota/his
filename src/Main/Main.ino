d7ba9080291d012dc54e62940366709a3fde9409
Don Luyendijk <donluyendijk@gmail.com>
2020-01-17 15:07:54 +0100

    added lcd

--- src/Main/Main.ino
+++ src/Main/Main.ino
@@ -1,10 +1,16 @@
-#define FANPIN 2
-#define TEMPSENSORIN A0
-#define TEMPSENSORVCC 4
-#define POTMETERPIN A1
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-unsigned int getPotmeterValue() {
-  return analogRead(POTMETERPIN);
+#define DEBUG
+
+#define FAN_PIN 2
+#define TEMPSENSOR_PIN A0
+#define TEMPSENSOR_VCC 4
+#define POTMETER_PIN A1
+
+unsigned int getPotmeterTemp() {
+  // TO DO: calibrate
+  return analogRead(POTMETER_PIN);
 }
 
 unsigned long toRpm(unsigned long pulseTime) {
@@ -14,42 +20,55 @@ unsigned long toRpm(unsigned long pulseTime) {
 
 int readTemp() {
   int temp;
-  digitalWrite(TEMPSENSORVCC, 1);
+  digitalWrite(TEMPSENSOR_VCC, 1);
   delay(5);
 
-  // in = 0 - 1023
-  // V = in / (1023 / 5)
-  // mV = V * 1000
-  // deg = mV / 10
-
+  // in = 0 - 1023        V = in / (1023 / 5)      mV = V * 1000       deg = mV / 10
   // deg = read/2.046
   temp = analogRead(A0) / 2.046;
-  digitalWrite(TEMPSENSORVCC, 0);
+  digitalWrite(TEMPSENSOR_VCC, 0);
   return temp;
 }
 
 unsigned long pulseTime;
 unsigned long lastInterrupt;
+char lcdBuffer[2][16];
+LiquidCrystal_I2C lcd(0x38, 16, 2);
 
 void isr() {
   static unsigned long start = 0;
-  if(!digitalRead(FANPIN)) start = micros();
-  if(digitalRead(FANPIN)) pulseTime = micros()-start;
+  if(!digitalRead(FAN_PIN)) start = micros();
+  if(digitalRead(FAN_PIN)) pulseTime = micros()-start;
   lastInterrupt = millis();
 }
 
 void setup() {
-  pinMode(7, OUTPUT);
-  digitalWrite(7, 1);
-  attachInterrupt(digitalPinToInterrupt(FANPIN), isr, CHANGE);
-  pinMode(TEMPSENSORIN, INPUT);
-  pinMode(TEMPSENSORVCC, OUTPUT);
-  pinMode(FANPIN, INPUT_PULLUP);
-  pinMode(POTMETERPIN, INPUT);
+  pinMode(TEMPSENSOR_PIN, INPUT);
+  pinMode(TEMPSENSOR_VCC, OUTPUT);
+  pinMode(FAN_PIN, INPUT_PULLUP);
+  pinMode(POTMETER_PIN, INPUT);
+  
+  attachInterrupt(digitalPinToInterrupt(FAN_PIN), isr, CHANGE);
   Serial.begin(9600);
+  lcd.init();
 }
 
 void loop() {
   if(lastInterrupt + 1000 < millis()) pulseTime = 0;
-  Serial.println("fan rpm: " + (String)toRpm(pulseTime) + " temperature: " + (String)readTemp() + " potmeter: " + (String)getPotmeterValue());
+
+  #ifdef DEBUG
+    Serial.println("fan rpm: " + (String)toRpm(pulseTime) + " temperature: " + (String)readTemp() + " potmeter: " + (String)getPotmeterTemp());
+  #endif
+  
+  memset(lcdBuffer[0], ' ', 16);
+  memset(lcdBuffer[1], ' ', 16);
+  snprintf(lcdBuffer[0], 16, "cur. temp: %-4d", readTemp());
+  snprintf(lcdBuffer[1], 16, "set temp: %-5d", getPotmeterTemp());
+
+  lcd.setCursor(0, 0);
+  lcd.print(lcdBuffer[0]);
+  lcd.setCursor(0, 1);
+  lcd.print(lcdBuffer[1]);
+
+  delay(100);
 }
